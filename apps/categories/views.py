8ad5ff87f07ffa86f1fecaaccdebe93781103e00
daryn <kapa.miki16@gmail.com>
2020-05-09 16:02:43 +0600

    FIX:

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -42,7 +42,8 @@ class CategoryViewSet(viewsets.ModelViewSet):
         serializer = serializer_class(instance=instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(status=status.HTTP_202_ACCEPTED, data=serializer.validated_data)
+        company_serializer = CompanySerializer(instance.company)
+        return Response(status=status.HTTP_202_ACCEPTED, data=company_serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer_class = self.get_serializer_class()
